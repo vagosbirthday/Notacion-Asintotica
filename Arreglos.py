1d81948a68930a0fb0b4ee7b2956a453d27886e6
Daniel <daniel.aguilar7255@gmail.com>
2024-08-06 16:28:51 -0600

    Se agregan comentarios

--- Arreglos.py
+++ Arreglos.py
@@ -1,12 +1,14 @@
 def encontrar_comunes(arr1, arr2):
+    # Convertimos las listas a conjuntos para eliminar duplicados y para buscar intersección
     set1 = set(arr1)
     set2 = set(arr2)
     
+    # Utilizamos la operación de intersección para encontrar los elementos comunes
     comunes = list(set1 & set2)
     
     return comunes
 
+# Ejemplo de uso
 arreglo_a = [3, 1, 7, 9, 2]
 arreglo_b = [4, 2, 8, 1, 5]
-print(encontrar_comunes(arreglo_a, arreglo_b)) 
-
+print(encontrar_comunes(arreglo_a, arreglo_b))  # Output: [1, 2]
